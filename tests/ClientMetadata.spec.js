4c60fc2b69a8384b62b70aab44b1c878dfde0514
Dmitri Zagidulin <dzagidulin@gmail.com>
2020-10-08 21:56:21 -0400

    Refactor ClientMetadata class.

--- tests/ClientMetadata.spec.js
+++ tests/ClientMetadata.spec.js
@@ -6,45 +6,39 @@ const chai = require('chai');
 chai.should();
 const {expect} = chai;
 
-const {
-  ClientMetadata,
-  DEFAULT_GRANT_TYPES,
-  DEFAULT_TOKEN_ENDPOINT_AUTH_METHOD
-} = require('../lib/ClientMetadata');
+const {ClientMetadata} = require('../lib/ClientMetadata');
 
 describe('ClientMetadata', () => {
   describe('constructor', () => {
     it('should create an instance with defaults', async () => {
-      const cm = new ClientMetadata();
+      const client = new ClientMetadata();
 
-      expect(cm.grant_types).to.eql(DEFAULT_GRANT_TYPES);
-      expect(cm.token_endpoint_auth_method)
-        .to.eql(DEFAULT_TOKEN_ENDPOINT_AUTH_METHOD);
+      expect(client.meta).to.eql({});
     });
   });
 
   describe('validateNew', () => {
     it('new instance should validate', async () => {
-      const cm = new ClientMetadata();
+      const client = new ClientMetadata();
 
-      const result = cm.validateNew();
+      const result = client.validateNew();
       expect(result.valid).to.be.true;
     });
   });
 
   describe('implicitFlow', () => {
     it('new instance should not be implicit flow by default', async () => {
-      const cm = new ClientMetadata();
+      const client = new ClientMetadata();
 
-      expect(cm.implicitFlow()).to.be.false;
+      expect(client.implicitFlow()).to.be.false;
     });
   });
 
   describe('requiresClientSecret', () => {
     it('new instance should require client secret by default', async () => {
-      const cm = new ClientMetadata();
+      const client = new ClientMetadata();
 
-      expect(cm.requiresClientSecret()).to.be.true;
+      expect(client.requiresClientSecret()).to.be.true;
     });
   });
 });
